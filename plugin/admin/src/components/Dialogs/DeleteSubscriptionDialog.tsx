b3f660841cd0435c33b959d7e8622dd5651d4c21
bocovo <juste@bocovo.me>
2023-07-23 23:08:18 +0100

    chore(admin): create modal to confirm subscription deletion

---
+++ plugin/admin/src/components/Dialogs/DeleteSubscriptionDialog.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Dialog,
+  DialogBody,
+  Flex,
+  Typography,
+  DialogFooter,
+  Button,
+} from "@strapi/design-system";
+import { ExclamationMarkCircle } from "@strapi/icons";
+import { Trash } from "@strapi/icons";
+
+interface DeleteSubscriptionDialogProps {
+  isVisible: boolean;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+const DeleteSubscriptionDialog = ({
+  isVisible,
+  onConfirm,
+  onCancel,
+}: DeleteSubscriptionDialogProps) => {
+  return (
+    <Dialog onClose={onCancel} title="Confirmation" isOpen={isVisible}>
+      <DialogBody icon={<ExclamationMarkCircle />}>
+        <Flex direction="column" alignItems="center" gap={2}>
+          <Flex justifyContent="center">
+            <Typography id="confirm-description">
+              Are you sure you want to delete this?
+            </Typography>
+          </Flex>
+        </Flex>
+      </DialogBody>
+
+      <DialogFooter
+        startAction={
+          <Button onClick={onCancel} variant="tertiary">
+            Cancel
+          </Button>
+        }
+        endAction={
+          <Button
+            variant="danger-light"
+            startIcon={<Trash />}
+            onClick={onConfirm}
+          >
+            Confirm
+          </Button>
+        }
+      />
+    </Dialog>
+  );
+};
+
+export default DeleteSubscriptionDialog;
