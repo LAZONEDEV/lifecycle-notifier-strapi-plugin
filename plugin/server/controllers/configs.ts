f95af7cbe543b4e88143f815c99ef32fa8894c31
bocovo <juste@bocovo.me>
2023-07-23 23:54:21 +0100

    chore(server): create service to get plugin's configs

---
+++ plugin/server/controllers/configs.ts
@@ -0,0 +1,17 @@
+import { Strapi } from "@strapi/strapi";
+import pluginId from "../../common/utils/pluginId";
+import { ConfigServiceApi } from "../services/config";
+
+export default ({ strapi }: { strapi: Strapi }) => ({
+  envRecipients(ctx) {
+    const service = strapi
+      .plugin(pluginId)
+      .service("config") as ConfigServiceApi;
+
+    const result = service.getRecipientConfig();
+    ctx.body = result;
+  },
+  index() {
+    return pluginId;
+  },
+});
