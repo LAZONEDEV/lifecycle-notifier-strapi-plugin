7efbf9d97157473a0f847da85cf61adf599b4ddb
bocovo <juste@bocovo.me>
2023-07-23 23:57:18 +0100

    chore(server): create `getAttachment` helper
    Create helper to get files that will be attached to mails

---
+++ plugin/server/helpers/getAttachments.ts
@@ -0,0 +1,41 @@
+import { MailAttachment, StrapiMedia } from "../types";
+import { fetchLocalMedia } from "../utils/fetchLocalMedia";
+import { getFileService } from "./getStrapiService";
+
+const fetchMedia = async (
+  media: StrapiMedia,
+  fileService: any
+): Promise<Blob | Buffer> => {
+  if (media.provider === "local") {
+    const buffer = fetchLocalMedia(media.url);
+    return buffer;
+  }
+  const signedMedia = await fileService.signFileUrls(media);
+
+  // @ts-ignore
+  const request = await fetch(signedMedia.url);
+  const blob = await request.blob();
+  const arrayBuffer = await blob.arrayBuffer()
+  return Buffer.from(arrayBuffer);
+};
+
+export const getAttachments = async (medias: StrapiMedia[]) => {
+  const fileService = getFileService();
+
+  const attachments: MailAttachment[] = [];
+  const fetchingPromises = medias.map((media) => {
+    return fetchMedia(media, fileService)
+      .then((blob) => {
+        attachments.push({
+          content: blob,
+          contentType: media.mime,
+          filename: media.name,
+        });
+      })
+      .catch(console.error);
+  });
+
+  await Promise.allSettled(fetchingPromises);
+
+  return attachments;
+};
