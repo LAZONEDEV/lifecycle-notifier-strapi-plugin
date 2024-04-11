6b3c9e87253c0e8ead2a0100fa9539f1c9374123
bocovo <bocovojuste@gmail.com>
2024-04-11 16:51:30 +0100

    refactor(server): extract listenerCollection updater
    Extract the update to make it easy to test

--- src/server/services/core/listenedCollectionUpdater.ts
+++ src/server/services/core/listenedCollectionUpdater.ts
@@ -7,36 +7,13 @@ import { notify } from "../../helpers/notify";
 import { getSubscriptionsForCollection } from "../../helpers/getSubsForCollection";
 import { getEntryWithRelation } from "../../helpers/getEntryWithRelation";
 import { CollectionEntry } from "../../types";
+import { updateListenedCollectionOnChangeOnSubsCollection } from "./updateListenedCollectionSet";
 
-export const listenCollectionUpdate = async () => {
-  strapi.db.lifecycles.subscribe(async (event) => {
-    const isModifyingEvent = ["beforeCreate", "afterDelete"].includes(
-      event.action
-    );
-    if (!isModifyingEvent) {
-      return;
-    }
 
-    const isSubscriptionCollection =
-      event.model.uid === subscriptionCollectionUid;
-    if (!isSubscriptionCollection) {
-      return;
-    }
-
-    const collectionName = (event?.params?.data as unknown as SubscriptionEntry)
-      ?.collectionName;
-
-    if (!collectionName) return;
-
-    if (event.action === "afterDelete") {
-      const existing = await getSubscriptionsForCollection(collectionName);
-      if (!existing.length) {
-        listenedCollection.delete(collectionName);
-      }
-    } else {
-      listenedCollection.add(collectionName);
-    }
-  });
+export const listenSubscriptionCollectionUpdate = async () => {
+  strapi.db.lifecycles.subscribe(
+    updateListenedCollectionOnChangeOnSubsCollection
+  );
 };
 
 export const loadExistingSubscriptions = async () => {
@@ -49,7 +26,7 @@ export const loadExistingSubscriptions = async () => {
   });
 };
 
-const handleEventSubscription: SubscriberFn = async (event) => {
+export const handleEventSubscription: SubscriberFn = async (event) => {
   const willSendNotification = !!(
     listenedCollection.has(event.model.uid) && new Set(Object.values(EventType))
   );
@@ -70,7 +47,7 @@ const handleEventSubscription: SubscriberFn = async (event) => {
   }
 };
 
-const notifyForSubscription = async (
+export const notifyForSubscription = async (
   subscription: SubscriptionEntry,
   entry: CollectionEntry
 ) => {
@@ -81,7 +58,11 @@ const notifyForSubscription = async (
     ];
 
     const entryWithRelations = relationsToPopulate.length
-      ? await getEntryWithRelation(subscription.collectionName, entry,relationsToPopulate)
+      ? await getEntryWithRelation(
+          subscription.collectionName,
+          entry,
+          relationsToPopulate
+        )
       : entry;
     if (!entryWithRelations) {
       return;
