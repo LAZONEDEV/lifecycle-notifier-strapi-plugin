4a55d0968ef9d6800f6a662b66ca911c092b60e8
bocovo <bocovojuste@gmail.com>
2024-04-15 12:41:15 +0100

    test(server): add test suite for handleEventSubscription

--- src/server/services/core/listenedCollectionUpdater.ts
+++ src/server/services/core/listenedCollectionUpdater.ts
@@ -1,12 +1,7 @@
-import { listenedCollection } from "../../constants/listenedCollection";
-import { EventType } from "../../../common/enums";
-import type { SubscriberFn } from "@strapi/database/lib/lifecycles/subscribers";
-import { getSubscriptionsForCollection } from "../../helpers/getSubsForCollection";
-import { CollectionEntry } from "../../types";
+
 import { updateListenedCollectionOnChangeOnSubsCollection } from "./updateListenedCollectionSet";
 import { getStrapi } from "../../helpers/getStrapi";
-import { notifyForSubscription } from "./notifyForSubscription";
-
+import { handleEventSubscription } from "./handleEventSubscription";
 
 export const listenSubscriptionCollectionUpdate = async () => {
   const strapi = getStrapi()
@@ -16,26 +11,7 @@ export const listenSubscriptionCollectionUpdate = async () => {
   );
 };
 
-export const handleEventSubscription: SubscriberFn = async (event) => {
-  const willSendNotification = !!(
-    listenedCollection.has(event.model.uid) && new Set(Object.values(EventType))
-  );
 
-  if (!willSendNotification) {
-    return;
-  }
-
-  const collectionUid = event.model.uid;
-  const createdEntry = event.result || event.params.data;
-
-  const relatedSubscriptions = await getSubscriptionsForCollection(
-    collectionUid
-  );
-
-  for (const relatedSubscription of relatedSubscriptions) {
-    notifyForSubscription(relatedSubscription, createdEntry as CollectionEntry);
-  }
-};
 
 
 
