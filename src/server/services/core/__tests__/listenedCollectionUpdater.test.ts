486e99250a8918f7c44b0aa69f24bdf55a426d1d
bocovo <bocovojuste@gmail.com>
2024-04-15 13:22:41 +0100

    test(server): tests for strapi db lifecycle subscribers

---
+++ src/server/services/core/__tests__/listenedCollectionUpdater.test.ts
@@ -0,0 +1,46 @@
+import { getStrapi } from "../../../helpers/getStrapi";
+import {
+  listenChangeOnCollection,
+  listenSubscriptionCollectionUpdate,
+} from "../listenedCollectionUpdater";
+import { updateListenedCollectionOnChangeOnSubsCollection } from "../updateListenedCollectionSet";
+
+jest.mock("../updateListenedCollectionSet", () => {
+  return {
+    updateListenedCollectionOnChangeOnSubsCollection: jest.fn(),
+  };
+});
+
+jest.mock("../../../helpers/getStrapi", () => ({
+  getStrapi: jest.fn().mockReturnValue({
+    db: {
+      lifecycles: {
+        subscribe: jest.fn(),
+      },
+    },
+  }),
+}));
+
+jest.mock("../handleEventSubscription",()=>({
+  handleEventSubscription: jest.fn()
+}))
+
+describe("tests suite for strapi db lifecycle subscribers", () => {
+  it("should subscribe listener for subscription update event", async () => {
+    const strapi = getStrapi();
+
+    await listenSubscriptionCollectionUpdate();
+
+    expect(strapi.db.lifecycles.subscribe).toHaveBeenCalledWith(
+      updateListenedCollectionOnChangeOnSubsCollection
+    );
+  });
+
+  it("should subscribe listener for global event", async () => {
+    const strapi = getStrapi();
+
+    await listenChangeOnCollection();
+
+    expect(strapi.db.lifecycles.subscribe).toHaveBeenCalled();
+  });
+});
--- src/server/services/core/listenedCollectionUpdater.ts
+++ src/server/services/core/listenedCollectionUpdater.ts
@@ -1,23 +1,18 @@
-
 import { updateListenedCollectionOnChangeOnSubsCollection } from "./updateListenedCollectionSet";
 import { getStrapi } from "../../helpers/getStrapi";
 import { handleEventSubscription } from "./handleEventSubscription";
 
 export const listenSubscriptionCollectionUpdate = async () => {
-  const strapi = getStrapi()
+  const strapi = getStrapi();
 
   strapi.db.lifecycles.subscribe(
     updateListenedCollectionOnChangeOnSubsCollection
   );
 };
 
-
-
-
-
 export const listenChangeOnCollection = async () => {
-  const strapi = getStrapi()
-  
+  const strapi = getStrapi();
+
   // @ts-ignore
   strapi.db.lifecycles.subscribe({
     async afterCreate(event) {
