4e47ce34e517cfe30aba727a57dbf76b3cd18f1b
bocovo <bocovojuste@gmail.com>
2024-04-15 14:10:35 +0100

    test(server): tests for utilities

---
+++ src/common/utils/__tests__/getCollectionNameFromUid.test.ts
@@ -0,0 +1,20 @@
+import { getCollectionNameFormUid } from "../getCollectionNameFormUid";
+
+describe("getCollectionNameFormUid", () => {
+  it("should return the collection name from a UID string", () => {
+    const uid = "api::planet.planet";
+    const expectedCollectionName = "planet";
+
+    const expected = getCollectionNameFormUid(uid);
+
+    expect(expected).toBe(expectedCollectionName);
+  });
+
+  it("should return an empty string if the UID is malformed", () => {
+    const malformedUid = "malformedUid";
+
+    const expected = getCollectionNameFormUid(malformedUid);
+
+    expect(expected).toBe(undefined);
+  });
+});
---
+++ src/server/utils/__test__/fetchLocalMedia.test.ts
@@ -0,0 +1,36 @@
+import { fetchLocalMedia } from "../fetchLocalMedia";
+import { readFileSync } from "node:fs";
+import { getStrapi } from "../../helpers/getStrapi";
+
+jest.mock("node:fs");
+jest.mock("../../helpers/getStrapi", () => ({
+  getStrapi: jest.fn(),
+}));
+
+describe("fetchLocalMedia", () => {
+  it("should read file synchronously from the correct path", () => {
+    const patch = "/example.jpg";
+    const publicDir = "/public";
+
+    const mockStrapi = {
+      dirs: {
+        static: {
+          public: publicDir,
+        },
+      },
+    };
+
+    (getStrapi as jest.Mock).mockReturnValue(mockStrapi);
+
+    const expectedPath = `${publicDir}${patch}`;
+    const mockFileContent = "mock file content";
+
+    (readFileSync as jest.Mock).mockReturnValueOnce(mockFileContent);
+
+    const result = fetchLocalMedia(patch);
+
+    expect(getStrapi).toHaveBeenCalled();
+    expect(readFileSync).toHaveBeenCalledWith(expectedPath);
+    expect(result).toBe(mockFileContent);
+  });
+});
