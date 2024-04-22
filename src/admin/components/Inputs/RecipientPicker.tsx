3be10d91fb0d6b3a569a86cf1357525e47e3de62
bocovo <bocovojuste@gmail.com>
2024-04-22 18:05:01 +0100

    refactor(web): some component
    Refactor of some component.

--- src/admin/components/Inputs/RecipientPicker.tsx
+++ src/admin/components/Inputs/RecipientPicker.tsx
@@ -7,11 +7,12 @@ import { RecipientOptionType } from "../../../common/types/index";
 import { RecipientType } from "../../../common/enums/index";
 import { getCollectionNameFormUid } from "../../../common/utils/getCollectionNameFormUid";
 import React from "react";
-import { ConfigService } from "../../services/Config";
 import {
   getRecipientStringValue,
   getRecipientTypeFromString,
 } from "../../utils/recipientStringValue";
+import { useRecipients } from "../../hooks/recipients";
+import { createRecipientLabel } from "../../utils/createRecipientLabel";
 
 export interface RecipientPickerProps extends FormFieldProps {
   collectionFormName: string;
@@ -29,23 +30,10 @@ export const RecipientPicker = ({
   const [customRecipients, setCustomRecipients] = useState<
     RecipientOptionType[]
   >([]);
-  const [envRecipients, setEnvRecipients] = useState<RecipientOptionType[]>([]);
+  const envRecipients = useRecipients();
   const [{ value: selectedCollectionName }] = useField(collectionFormName);
   const [{ value }, { error }, { setValue }] =
     useField<RecipientOptionType[]>(name);
-
-  const loadEnvRecipients = async () => {
-    const envsNames = await ConfigService.getEnvRecipients();
-    if(!envsNames){
-      return;
-    }
-
-    const recipientOptions = envsNames.map<RecipientOptionType>((env) => ({
-      type: RecipientType.ENV,
-      value: env,
-    }));
-    setEnvRecipients(recipientOptions);
-  };
 
   useEffect(() => {
     if (value) {
@@ -57,10 +45,6 @@ export const RecipientPicker = ({
         ...includedCustomsValues,
       ]);
     }
-  }, []);
-
-  useEffect(() => {
-    loadEnvRecipients();
   }, []);
 
   const modelEmailFields = useCollectionFieldType({
@@ -109,14 +93,7 @@ export const RecipientPicker = ({
           const value = getRecipientStringValue(field);
           return (
             <Option key={value} value={value}>
-              {(() => {
-                if (field.type === RecipientType.CUSTOM) {
-                  return field.value;
-                } else if (field.type === RecipientType.ENV) {
-                  return `ENV.${field.value}`;
-                }
-                return `${collectionName}.${field.value}`;
-              })()}
+              {createRecipientLabel(field, collectionName)}
             </Option>
           );
         }
---
+++ src/admin/hooks/getCollections.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { CollectionSchema } from "../types";
+import { loadCollectionsSchemas, filterApiCollection } from "../utils/loadCollections";
+
+export const useCollections = () => {
+  const [collections, setCollections] = useState<CollectionSchema[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const [request, abort] = loadCollectionsSchemas();
+    request.then((result) => {
+      if (result) {
+        const apiCollections = filterApiCollection(result.data);
+        setCollections(apiCollections);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      abort();
+    };
+  }, []);
+
+  return {collections, loading}
+}
