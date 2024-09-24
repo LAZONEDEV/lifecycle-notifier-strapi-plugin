b78e9005c8fa5e5e2cc77dc12852a0d96336a2f9
bocovo <bocovojuste@gmail.com>
2024-09-24 11:51:25 +0100

    chore(server): retrieve subscriptions according to the triggered event
    Retrieve subscriptions according to the triggered event

--- src/server/helpers/getSubsForCollection.ts
+++ src/server/helpers/getSubsForCollection.ts
@@ -1,13 +1,21 @@
 import { subscriptionCollectionUid } from "../../common/constants";
+import { EventType } from "../../common/enums";
 import { SubscriptionEntry } from "../../common/types";
 import { getStrapi } from "./getStrapi";
 
 export const getSubscriptionsForCollection = async (
-  collectionUid: string
+  collectionUid: string,
+  action?: EventType,
 ): Promise<SubscriptionEntry[]> => {
   const strapi = getStrapi()
 
+  const filterQuery: Record<string, string> = { collectionName: collectionUid }
+
+  if(action){
+    filterQuery.eventType = action;
+  }
+
   return await strapi.db
     .query(subscriptionCollectionUid)
-    .findMany({ where: { collectionName: collectionUid } });
+    .findMany({ where: filterQuery });
 };
