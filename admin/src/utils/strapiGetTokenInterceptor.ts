fa2247d161a96b5387835837c00e45df678360f2
bocovo <bocovojuste@gmail.com>
2024-11-20 07:53:15 +0100

    fix(amin): remove hook usage in non component function

--- admin/src/utils/strapiGetTokenInterceptor.ts
+++ admin/src/utils/strapiGetTokenInterceptor.ts
@@ -1,16 +1,10 @@
-import { useStrapiToken } from './useStrapitoken';
-
 export const strapiGetTokenInterceptor = (options: RequestInit) => {
-  const token = useStrapiToken();
-  console.log('token', token);
-
   if (!options.headers) {
     options.headers = {} as Record<string, string>;
   }
 
   options.headers = {
     ...options.headers,
-    Authorization: `Bearer ${token}`,
   };
 
   return options;
