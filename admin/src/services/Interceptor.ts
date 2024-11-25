12942ea198985852058abb5fc3c15defa57bb43c
bocovo <bocovojuste@gmail.com>
2024-11-25 07:14:24 +0100

    refactor(admin): avoid redundancy on the way the auth token is set

--- admin/src/services/Interceptor.ts
+++ admin/src/services/Interceptor.ts
@@ -2,8 +2,8 @@ import { apiRoutes } from '../constants/apiRoutes';
 import fetchInstance from '../utils/fetchInstance';
 
 export class InterceptorService {
-  static async getInterceptors() {
-    const result = await fetchInstance.get(apiRoutes.pluginInterceptors);
+  static async getInterceptors(authToken: string) {
+    const result = await fetchInstance.withAuthGet(apiRoutes.pluginInterceptors, authToken);
 
     return result as string[];
   }
