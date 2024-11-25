cbb470ca93dde30b7e94c33cf714a7dc0ca6d7a3
bocovo <bocovojuste@gmail.com>
2024-11-25 07:14:24 +0100

    refactor(admin): avoid redundancy on the way the auth token is set

--- admin/src/services/Config.ts
+++ admin/src/services/Config.ts
@@ -2,8 +2,11 @@ import { apiRoutes } from '../constants/apiRoutes';
 import fetchInstance from '../utils/fetchInstance';
 
 export class ConfigService {
-  static async getEnvRecipients() {
-    const result = await fetchInstance.get<string[]>(apiRoutes.pluginEnvRecipients);
+  static async getEnvRecipients(authToken: string) {
+    const result = await fetchInstance.withAuthGet<string[]>(
+      apiRoutes.pluginEnvRecipients,
+      authToken
+    );
     return result;
   }
 }
--- admin/src/services/Subscription.ts
+++ admin/src/services/Subscription.ts
@@ -5,30 +5,26 @@ import instance from '../utils/fetchInstance';
 export class SubscriptionService {
   static baseUrl = apiRoutes.pluginSubscriptions;
   static async get(token: string) {
-    const subs = await instance.get(SubscriptionService.baseUrl, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const subs = await instance.withAuthGet(SubscriptionService.baseUrl, token);
     return subs.results as SubscriptionEntry[];
   }
 
   static async create(data: Omit<SubscriptionEntry, 'id'>, token: string) {
-    const result = await instance.post<SubscriptionEntry>(SubscriptionService.baseUrl, data, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const result = await instance.withAuthPost<SubscriptionEntry>(
+      SubscriptionService.baseUrl,
+      token,
+      data
+    );
     return result;
   }
 
   static async delete(id: string, token: string) {
-    const result = await instance.delete(`${SubscriptionService.baseUrl}/${id}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const result = await instance.withAuthDelete(`${SubscriptionService.baseUrl}/${id}`, token);
     return result.data;
   }
 
   static async update(id: string, data: Omit<SubscriptionEntry, 'id'>, token: string) {
-    const result = await instance.put(`${SubscriptionService.baseUrl}/${id}`, data, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const result = await instance.withAuthPut(`${SubscriptionService.baseUrl}/${id}`, token, data);
     return result.data;
   }
 }
